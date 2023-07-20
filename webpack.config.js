ed9c3941c9c3d0acd24929b13c84013b1761b71b
Bohdana <danaatatum@gmail.com>
2023-07-20 14:11:59 +0300

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,46 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './webpage/js/index.js', // Встановіть свій власний вхідний файл
   output: {
-    path: path.resolve(__dirname, 'webpage'), // Встановіть папку виводу
+    path: path.resolve(__dirname, 'dist'), // Встановіть папку виводу
     filename: 'script.js', // Встановіть назву виводного файлу
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Використовуємо Babel для транспіляції коду ES6
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
         ],
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './webpage/index.html', // Шаблон для головної HTML сторінки
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Назва виводного CSS файлу
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8000,
   },
 };
