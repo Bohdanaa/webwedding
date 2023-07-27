146c4ded55805c1aab99a0f6108bad1d67a1296a
Bohdana <danaatatum@gmail.com>
2023-07-27 13:57:31 +0300

    change index.js - fetch(https)

--- webpage/js/index.js
+++ webpage/js/index.js
@@ -6,37 +6,37 @@ const form = document.querySelector('form');
 
 // Обробник події для відправки форми
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Заборонити стандартну відправку форми
+	event.preventDefault(); // Заборонити стандартну відправку форми
 
-  // Отримати значення полів форми
-  const name = document.getElementById('name').value;
-  const presence = document.getElementById('presence').value;
-  const drinks = Array.from(document.querySelectorAll('input[name="drinks"]:checked')).map((checkbox) => checkbox.value);
-  const car = Array.from(document.querySelectorAll('input[name="car"]:checked')).map((checkbox) => checkbox.value);
+	// Отримати значення полів форми
+	const name = document.getElementById('name').value;
+	const presence = document.getElementById('presence').value;
+	const drinks = Array.from(document.querySelectorAll('input[name="drinks"]:checked')).map((checkbox) => checkbox.value);
+	const car = Array.from(document.querySelectorAll('input[name="car"]:checked')).map((checkbox) => checkbox.value);
 
-  // Відправити дані на сервер
-  fetch('/submit', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name, presence, drinks, car }),
-  })
-    
-    .then(function(response) {
+	// Відправити дані на сервер
+	fetch('https://wedding-my.netlify.app/submit', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ name, presence, drinks, car }),
+	})
 
-      // Додати додаткові дії після відправки форми, якщо потрібно
-      const successMessage = response.data.message;
-    
-      // Відображення сповіщення про успішне відправлення
-      successMessage.textContent = successMsg;
-      
-      // Очищення форми або здійснення інших дій
-      form.reset();
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      // Обробити помилку відправки форми, якщо потрібно
-    });
+		.then(function (response) {
+
+			// Додати додаткові дії після відправки форми, якщо потрібно
+			const successMessage = response.data.message;
+
+			// Відображення сповіщення про успішне відправлення
+			successMessage.textContent = successMsg;
+
+			// Очищення форми або здійснення інших дій
+			form.reset();
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+			// Обробити помилку відправки форми, якщо потрібно
+		});
 });
 
